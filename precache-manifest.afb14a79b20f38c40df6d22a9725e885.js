5da2796b06203210fd0f40523e6cedbfd3abea29
Erin Kang <erin@apposter.com>
2020-02-26 12:30:43 +0900

    Updates

---
+++ precache-manifest.afb14a79b20f38c40df6d22a9725e885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24f23abd1184d31f79b115599279f8ab",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7621a9cab8339f6c8042",
+    "url": "/movie_app/static/css/main.89b9412a.chunk.css"
+  },
+  {
+    "revision": "be72762f97654a4a8ea8",
+    "url": "/movie_app/static/js/2.06ea5970.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.06ea5970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7621a9cab8339f6c8042",
+    "url": "/movie_app/static/js/main.4c153120.chunk.js"
+  },
+  {
+    "revision": "c0057b52e72b0f614c37",
+    "url": "/movie_app/static/js/runtime-main.15be7292.js"
+  }
+]);
